5f70f8b3c5aa43380b0fd45b57fe49b0d8f16bdd
Phil Austin <paustin@eos.ubc.ca>
2018-12-16 08:46:38 -0800

    clean up pre-commit

--- scripts/pre_commit_jupytext.py
+++ scripts/pre_commit_jupytext.py
@@ -1,40 +1,28 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.2'
-#       jupytext_version: 0.8.6
-# ---
-
-# %%
+"""
+this script is used by
+https://github.com/pre-commit/pre-commit
+to convert notebooks to python/*py files and add them
+to the index pre-commit
+"""
 import sys
+import jupytext
 import subprocess
 from pathlib import Path
-import shutil
 
-
-cmd = f"jupytext --to py:percent {sys.argv[1]}"
-notebook = Path(sys.argv[1])
-status1, output1 = subprocess.getstatusoutput(cmd)
-pyfile = notebook.with_suffix(".py")
+nbfile = Path(sys.argv[1])
+pyfile = nbfile.with_suffix(".py")
+nb = jupytext.readf(nbfile)
 pydir = pyfile.parent / Path("python")
 pydir.mkdir(parents=True, exist_ok=True)
-newfile = pydir / pyfile.name
-shutil.move(str(pyfile.resolve()), str(newfile.resolve()))
-cmd = f"git add {newfile}"
-# print(f"working with {newfile}")
-status2, output2 = subprocess.getstatusoutput(cmd)
+pypath = pydir / pyfile.name
+jupytext.writef(nb, pypath, format="percent")
+cmd = f"git add {str(pypath)}"
+status1, output1 = subprocess.getstatusoutput(cmd)
 with open("jupytext_logfile.txt", "w") as f:
     f.write(
         f"""
-         {sys.argv}
-         {status1}
-         {output1}
-         {status2}
-         {output2}
-         writing out {newfile}
+         notebook: {sys.argv[1]}
+         writing out to {pypath}
         """
     )
 sys.exit(0)
