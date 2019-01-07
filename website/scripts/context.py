eed4107c8451809315a297ee917a76c8df58c9f7
Phil Austin <paustin@eos.ubc.ca>
2019-01-06 18:28:45 -0800

    add metadata injector for nbsphinx

--- website/scripts/context.py
+++ website/scripts/context.py
@@ -1,12 +1,18 @@
 """
 http://docs.python-guide.org/en/latest/writing/structure
 """
+import site
+import sys
 from pathlib import Path
 
 curr_dir = Path(__file__).parent
+root_dir = curr_dir.parents[1]
+notebook_dir = root_dir / Path("notebooks")
+website_dir = root_dir / Path("website")
+json_path = website_dir / Path("notebooks.json")
 
 # can_path = Path(os.environ['HOME']) / Path('repos/canvas_scripting')
 # sys.path.insert(0, can_path)
-# sep = "*" * 30
-# print(f"{sep}\ncontext imported. Front of path:\n{sys.path[0]}\n{sys.path[1]}\n{sep}\n")
-# site.removeduppaths()
+sep = "*" * 30
+print(f"{sep}\ncontext imported. Front of path:\n{sys.path[0]}\n{sys.path[1]}\n{sep}\n")
+site.removeduppaths()
