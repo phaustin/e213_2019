5c77a616116d8867340f76a2851eb512f9b532c9
Phil Austin <paustin@eos.ubc.ca>
2019-02-21 09:39:12 -0800

    new file

---
+++ website/web_notebooks/python/cross_link_demo.py
@@ -0,0 +1,31 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: all
+#     formats: ipynb
+#     notebook_metadata_filter: all
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.2'
+#       jupytext_version: 1.0.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+# %% [markdown] {"toc": true}
+# <h1>Table of Contents<span class="tocSkip"></span></h1>
+# <div class="toc"><ul class="toc-item"><li><span><a href="#Cross-reference-examples" data-toc-modified-id="Cross-reference-examples-1"><span class="toc-item-num">1&nbsp;&nbsp;</span>Cross reference examples</a></span><ul class="toc-item"><li><span><a href="#2D-transient-diffusion" data-toc-modified-id="2D-transient-diffusion-1.1"><span class="toc-item-num">1.1&nbsp;&nbsp;</span>2D transient diffusion</a></span><ul class="toc-item"><li><span><a href="#Flux-boundary-conditions" data-toc-modified-id="Flux-boundary-conditions-1.1.1"><span class="toc-item-num">1.1.1&nbsp;&nbsp;</span>Flux boundary conditions</a></span></li></ul></li></ul></li></ul></div>
+# %% [markdown]
+# # Cross reference examples
+#
+# Show how to link out of a notebook into a review document.
+# %% [markdown]
+# ## 2D transient diffusion
+#
+# Recall our example timestep, shown [in this link](review_page.html#detaileexample)
+# %% [markdown]
+# ### Flux boundary conditions
+#
+# Remember that we specifiy a flux boundary condition [this way](review_page.html#fluxboundary)
