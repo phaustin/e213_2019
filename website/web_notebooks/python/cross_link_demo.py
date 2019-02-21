51d7d3122e5a17e04e8cfd3b2f3ad5319208595e
Phil Austin <paustin@eos.ubc.ca>
2019-02-21 10:14:24 -0800

    add cross reference to another notebook

--- website/web_notebooks/python/cross_link_demo.py
+++ website/web_notebooks/python/cross_link_demo.py
@@ -16,11 +16,13 @@
 # ---
 # %% [markdown] {"toc": true}
 # <h1>Table of Contents<span class="tocSkip"></span></h1>
-# <div class="toc"><ul class="toc-item"><li><span><a href="#Cross-reference-examples" data-toc-modified-id="Cross-reference-examples-1"><span class="toc-item-num">1&nbsp;&nbsp;</span>Cross reference examples</a></span><ul class="toc-item"><li><span><a href="#2D-transient-diffusion" data-toc-modified-id="2D-transient-diffusion-1.1"><span class="toc-item-num">1.1&nbsp;&nbsp;</span>2D transient diffusion</a></span><ul class="toc-item"><li><span><a href="#Flux-boundary-conditions" data-toc-modified-id="Flux-boundary-conditions-1.1.1"><span class="toc-item-num">1.1.1&nbsp;&nbsp;</span>Flux boundary conditions</a></span></li></ul></li></ul></li></ul></div>
+# <div class="toc"><ul class="toc-item"><li><span><a href="#Cross-reference-examples" data-toc-modified-id="Cross-reference-examples-1"><span class="toc-item-num">1&nbsp;&nbsp;</span>Cross reference examples</a></span><ul class="toc-item"><li><span><a href="#2D-transient-diffusion" data-toc-modified-id="2D-transient-diffusion-1.1"><span class="toc-item-num">1.1&nbsp;&nbsp;</span>2D transient diffusion</a></span><ul class="toc-item"><li><span><a href="#Flux-boundary-conditions" data-toc-modified-id="Flux-boundary-conditions-1.1.1"><span class="toc-item-num">1.1.1&nbsp;&nbsp;</span>Flux boundary conditions</a></span></li></ul></li><li><span><a href="#Here-is-a-reference-to-a-section-in-another-ipynb-file" data-toc-modified-id="Here-is-a-reference-to-a-section-in-another-ipynb-file-1.2"><span class="toc-item-num">1.2&nbsp;&nbsp;</span>Here is a reference to a section in another ipynb file</a></span></li></ul></li></ul></div>
 # %% [markdown]
 # # Cross reference examples
 #
 # Show how to link out of a notebook into a review document.
+# %% [markdown]
+# ## Here is a reference to a rst file
 # %% [markdown]
 # ## 2D transient diffusion
 #
@@ -29,3 +31,7 @@
 # ### Flux boundary conditions
 #
 # Remember that we specifiy a flux boundary condition [this way](review_page.html#fluxboundary)
+# %% [markdown]
+# ## Here is a reference to a section in another ipynb file
+# %% [markdown]
+# And recall Section 5 of the [6_finite_volume_5.ipynb notebook](6_finite_volume_5.ipynb#5---gridblock-example)
