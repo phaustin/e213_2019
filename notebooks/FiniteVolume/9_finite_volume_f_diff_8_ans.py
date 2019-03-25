8000229f5dd626399539ec779d83259ab667a971
Roger Beckie <44749906+rbeckie@users.noreply.github.com>
2019-03-24 20:04:28 -0700

    small typo correction
    the function in markdown was a quadratic but the actual function was a cubic

--- notebooks/FiniteVolume/9_finite_volume_f_diff_8_ans.py
+++ notebooks/FiniteVolume/9_finite_volume_f_diff_8_ans.py
@@ -8,11 +8,33 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.2'
-#       jupytext_version: 1.0.2
+#       jupytext_version: 1.0.3
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
 #     name: python3
+#   language_info:
+#     codemirror_mode:
+#       name: ipython
+#       version: 3
+#     file_extension: .py
+#     mimetype: text/x-python
+#     name: python
+#     nbconvert_exporter: python
+#     pygments_lexer: ipython3
+#     version: 3.6.5
+#   toc:
+#     base_numbering: 1
+#     nav_menu: {}
+#     number_sections: true
+#     sideBar: true
+#     skip_h1_title: false
+#     title_cell: Table of Contents
+#     title_sidebar: Contents
+#     toc_cell: false
+#     toc_position: {}
+#     toc_section_display: true
+#     toc_window_display: false
 # ---
 
 # %% [markdown]
@@ -63,7 +85,7 @@
 # Let's try the following function which is easy to plot and differentiate:
 # <br><br>
 # \begin{align}
-# f(x)&= -(x - 2.5)^2 + 5
+# f(x)&= -(x - 2.5)^3 + 5
 # \end{align}
 # <br><br>
 # We can implement it in python below as `func_1`.
