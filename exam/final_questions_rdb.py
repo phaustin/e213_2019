e5f376a1c8febbd2c2617c860dbf4ba22b804ba8
Roger Beckie <44749906+rbeckie@users.noreply.github.com>
2019-04-12 09:13:14 -0700

    Update final_questions_rdb.py
    Revised question

--- exam/final_questions_rdb.py
+++ exam/final_questions_rdb.py
@@ -25,17 +25,32 @@
 # - can represent a model for a discrete control volume as either a difference equation or a differential equation.
 
 # %% [markdown]
-# **Acidity** is is an important water-quality parameter at mine sites. It describes the moles of a base (typically carbonate) required to raise a water's pH to a prescribed value. Acidity is a **conserved quantity**. In practice, the units of are moles of acidity per litre of water $[M/L^3]$. Acidity is usually generated from the oxidation of pyrite (iron sulfide), producing acid. The rate of production depends upon oxygen levels, pH, temperature and pyrite grain size. 
+# **Acidity** is is an important water-quality parameter at mine sites. It describes the moles of a base (typically carbonate) required to raise a water's pH to a prescribed value. Acidity is a **conserved quantity**. In practice, the units are moles of acidity per litre of water $[M/L^3]$. Acidity is usually generated from the oxidation of pyrite (iron sulfide), which produces acid.  
 #
-# In this question, you will develop an equation (model) that describes the change in acidity over a time period $\Delta t$ in a tailings management facility (TMF or tailings pond), under these assumptions:
+# In this question, you will develop a model (equations) that describe the change in acidity over time in a tailings management facility (TMF or tailings pond), under these assumptions:
 # <br>
 # 1. the TMF has a total volume of $V(t)~[L^3]$, that changes in time in response to in- and outflows of water.
 # * drainage from the pit flows into the TMF at a rate $Q_{pit}(t)~[L^3/T]$ with an acidity concentration of $c_{pit}~[M/L^3]$, measured in units of moles acidity per litre.
-# * precipitation with zero acidity $c_{precip} = 0~ moles/l$ enters the TMF as precipitation on the surface of the TMF. Assume that the precipitation rate is given from data and can be represented in your equation as $P(t),~[L/T]$ with units of $mm/day$. Assume that the surface area of the TMF is $A~[L^2]$ and **does not change** as the volume in the TMF changes (an approximation that is valid for small changes in volume).
+# * precipitation with zero acidity $c_{precip} = 0~ moles/l$ enters the TMF as precipitation on the surface of the TMF. Assume that the precipitation rate is given from data and can be represented in your model equations as $P(t),~[L/T]$ with units of $mm/day$. Assume that the surface area of the TMF is $A~[L^2]$ with units of $m^2$. Assume that area **does not change** as the volume in the TMF changes (an approximation that is valid for small changes in volume). 
 # * evaporation removes water from the TMF at a rate that can be represented in your equation as $ET(t)~[L/T]$ with units of $mm/day$. 
-# * the TMF is well stirred, such that the acidity in the TMF, $c_{TMF}~[M/L^3]$, measured in units of moles acidity per litre, is the same at all points in the pond at each instant in time.
+# * the TMF is well stirred, such that the concentration of acidity in the TMF, $c_{TMF}~[M/L^3]$, measured in units of moles acidity per litre of water, is the same at all points in the pond at each instant in time.
+#
 # * water is discharged from the TMF at a rate $Q_{dis}(t)~[L^3/T]$ with an acidity concentration of $c_{TMF}$.
 # * assume all other sources and sinks of water are negligible and can be ignored.
+#
+# | Variable    |    Description    |Dimension| 
+# |----------   |      -------------|   ---------|
+# | $V(t)$          | Volume in the TMF| $[L^3]$|
+# | $Q_{pit}(t)$    | Rate of flow from pit into TMF| $[L^3/T]$|
+# | $c_{pit}(t)$    | Concentration of acidity in pit water| $[M/L^3]$|
+# | $P(t)$          | Precipitation rate                    | $[L/T]$ |
+# | $ET(t)$         | Evaporation rate                    | $[L/T]$  |
+# | $A$             | Surface area of the TMF m           | $[L^2]$  |
+# | $c_{TMF}(t)$       | Concentration of acidity in the TMF | $[M/L^3]$ |
+# | $Q_{dis}(t)$  | Rate of discharge out of TMF      | $[L^3/T]$|
+#
+#
+#
 #
 # Question a.
 #
@@ -60,3 +75,8 @@
 # Question d.
 #
 # Write a differential equation, or differential equations for the **concentration** of acidity in the TMF $c_{TMF}(t)$.
+#
+# Question e.
+#
+# Acidity can be generated from oxidation of pyrite in the tailings at the bottom of the pond. If the acidity flux per unit area of TMF bottom  $j_{acid}~[M/(L^2 T)]$ is given by $j_{acid} = k c_{TMF} + b$. What are the dimensions of the parameters $k$ and $b$?
+#
