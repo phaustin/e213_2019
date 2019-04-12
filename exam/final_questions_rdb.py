5f102d0684876bb1c583dbaa44cc025d68ed801c
Phil Austin <paustin@eos.ubc.ca>
2019-04-12 12:15:26 -0700

    move table

--- exam/final_questions_rdb.py
+++ exam/final_questions_rdb.py
@@ -7,12 +7,38 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.2'
-#       jupytext_version: 1.0.2
+#       jupytext_version: 1.0.4
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
 #     name: python3
+#   language_info:
+#     codemirror_mode:
+#       name: ipython
+#       version: 3
+#     file_extension: .py
+#     mimetype: text/x-python
+#     name: python
+#     nbconvert_exporter: python
+#     pygments_lexer: ipython3
+#     version: 3.6.7
+#   toc:
+#     base_numbering: 1
+#     nav_menu: {}
+#     number_sections: true
+#     sideBar: true
+#     skip_h1_title: false
+#     title_cell: Table of Contents
+#     title_sidebar: Contents
+#     toc_cell: true
+#     toc_position: {}
+#     toc_section_display: true
+#     toc_window_display: true
 # ---
+
+# %% [markdown] {"toc": true}
+# <h1>Table of Contents<span class="tocSkip"></span></h1>
+# <div class="toc"><ul class="toc-item"></ul></div>
 
 # %% [markdown]
 # I'd like to test their abilities across the math/conceptual spectrum, from the simple box models to PDEs.
